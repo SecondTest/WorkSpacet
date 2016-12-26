93be9bedbcd48b31a0eee81b291cc2e4e48d7e14
Guoyongbo <780408645@qq.com>
2016-12-26 16:15:11 +0800

    first commit

---
+++ app/src/main/java/com/example/administrator/vvv/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.vvv;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    private TextView name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
